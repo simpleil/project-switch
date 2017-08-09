8170c367c6607b2b5f42420ff23e63ec07f1cd81
simplel <tanya_0308@ukr.net>
2017-08-09 23:17:02 +0300

    +mixItUp and bootstrapValidationnForm

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("a").on('click', function(event) {
+    $('a').on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
@@ -12,5 +12,28 @@ $(document).ready(function(){
     } 
   });
 
-     $('.bxslider').bxSlider();
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.scroll_to_top').fadeIn();
+    } else {
+      $('.scroll_to_top').fadeOut();
+    }
+  });
+  
+  $('.scroll_to_top').click(function(){
+    $('html, body').animate({scrollTop : 0}, 1000);
+    return false;
+  });
+
+  $('.bxslider').bxSlider();
+
+  $('.worksNav li').on('click', function(){
+    $('.worksNav li').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  });
+
+  mixitup($('.worksWrapper'));
+
+     
 });
